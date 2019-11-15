436acb7eb31114acb294da2b34bccf415fb91b74
FJ <fsanchez@lsi.uned.es>
2019-11-15 09:05:24 +0100

    Inicializa la Versión 1.0

--- README.md
+++ README.md
@@ -1,2 +1,132 @@
-# PF
-Herramienta de extracción de contenidos de los Foros aLF
+# Extracción de contenidos de los Foros aLF
+
+_Herramienta de obtención y generación de contenidos estructurados de los Foros académicos de las asignaturas del 
+entorno de aprendizaje aLF de la Universidad Nacional de Educación a Distancia (UNED) de España._
+
+## Instrucciones ⚙
+
+_Utilizar un entorno de desarrollo que admita el control de versiones (VCS) compatible Git, o que permita clonar, 
+importar o compartir un proyecto GitHub nativo._
+
+_Estas instrucciones te permitirán obtener una copia del proyecto en funcionamiento en tu máquina local para propósitos de desarrollo y pruebas._
+
+Mira **Despliegue** para conocer cómo desplegar el proyecto.
+
+
+### Pre-requisitos 📋
+
+* _Python 3_
+
+```
+    > python --version
+    > Python 3.x.x
+```
+
+* [_requirements.txt_](https://github.com/IA-LA/PF/requirements.txt) - Listado de dependencias
+
+```
+    > pip list
+```
+
+### Instalación 🔧
+
+_Una serie de ejemplos paso a paso explica lo que debe ejecutarse para tener un entorno de desarrollo de la herramienta_
+
+_Primer paso: instalación de dependencias_
+
+```
+    > pip install -r requirements.txt
+```
+
+_Y comprobación de las dependencias_
+
+```
+    > pip freeze
+    > pip list
+    anytree==2.6.0
+    beautifulsoup4==4.6.0
+    bs4==0.0.1
+    colorama==0.3.9
+    decorator==4.2.1
+    et-xmlfile==1.0.1
+    graphviz==0.11.1
+    html5lib==1.0.1
+    ipython==6.2.1
+    ipython-genutils==0.2.0
+    jdcal==1.4
+    jedi==0.11.1
+    nltk==3.4
+    numpy==1.16.2
+    openpyxl==2.6.2
+    pandas==0.24.2
+    parso==0.1.1
+    pickleshare==0.7.4
+    prompt-toolkit==1.0.15
+    Pygments==2.2.0
+    python-dateutil==2.8.0
+    pytz==2018.9
+    simplegeneric==0.8.1
+    singledispatch==3.4.0.3
+    six==1.12.0
+    stats==0.1.2a0
+    traitlets==4.3.2
+    wcwidth==0.1.7
+    webencodings==0.5.1
+```
+
+_Ejemplo de cómo obtener datos del sistema o como usarlos para una pequeña demo_
+
+```
+    > python
+     "C:\Users\FJ\PycharmProjects\foros\forosGitHub\parserForos\foros\ForosR____ABC____0123456____2019.txt" "0123456"
+```
+
+## Despliegue ⌨
+
+_Agrega notas adicionales sobre cómo hacer el despliegue_
+
+```
+    > pip install -r requirements.txt
+```
+
+## Construido con 🛠️
+
+_Menciona las herramientas que utilizadas para crear el proyecto_
+
+* [Python](https://www.python.org/) - El lenguaje usado
+* [Pip](https://pypi.org/project/pip/) - Manejador de dependencias
+* [Pycharm](https://www.jetbrains.com/pycharm/) - El framework web utilizado
+
+## Contribuyendo 🖇️
+
+Por favor lee el [CONTRIBUTING.md](https://github.com/IA-LA/PF/CONTRIBUTING.md) para detalles de nuestro código de conducta, y el proceso para el envío de solicitudes de pull.
+
+## Wiki 📖
+
+Puedes encontrar mucho más de cómo utilizar este proyecto en nuestra [Wiki](https://github.com/IA-LA/PF/wiki)
+
+## Versionado 📌
+
+Se utilizará [SemVer](http://semver.org/) para el versionado. Para todas las versiones disponibles, mira los [tags en este repositorio](https://github.com/IA-LA/PF/tags).
+
+## Autores ✒
+
+_A todos aquellos que ayudaron a levantar el proyecto desde sus inicios_
+
+* **Aitor Díaz** - [*Versión 1.0*](https://github.com/IA-LA/PF/v1.0) - [aitordiaz](aitordiaz@pas.uned.es)
+* **Álvaro Rodrigo** - [alvarory](alvarory@lsi.uned.es)
+* **Francisco Sánchez** - [*Versión 1.1*](https://github.com/IA-LA/PF/v1.1) - [fsanchez](https://github.com/IA-LA/PF/v1.1)
+* **Francisco Sánchez** - [*Versión 1.2*](https://github.com/IA-LA/PF/v1.2) - [fsanchez](https://github.com/IA-LA/PF/v1.2)
+* **José Luis F. Vindel** - [jlvindel](jlvindel@dia.uned.es)
+
+También puede consultarse la lista de todos los [contribuyentes](https://github.com/IA-LA/PF/contributors) que han participado en este proyecto. 
+
+## Licencia 📄
+
+Este proyecto está bajo la Licencia (GNU 3) - mira el archivo [LICENCIA](LICENSE) para más detalles
+
+
+
+
+---
+⌨ con ❤ por [fsanchez](https://github.com/IA-LA) 😊
---
+++ v1.0/ParserPlano.py
@@ -0,0 +1,162 @@
+""""
+Created on 09-01-2019
+
+@author: Aitor Diaz Medina
+
+Clase principal encargada llamar al parser y generar el archivo csv resultante
+
+"""
+
+import hashlib
+import csv
+from ConvertirUTF8 import *
+
+
+def month_string_to_number(string):
+    m = {
+        'ene': 1,
+        'feb': 2,
+        'mar': 3,
+        'abr': 4,
+        'may': 5,
+        'jun': 6,
+        'jul': 7,
+        'ago': 8,
+        'sep': 9,
+        'oct': 10,
+        'nov': 11,
+        'dic': 12
+    }
+    s = string.strip()[:3].lower()
+
+    out = m[s]
+    return out
+
+
+def parserPlano(ruta, id_asig):
+    with open(ruta, 'r', encoding='utf8') as f:
+        lineas = f.readlines()
+        lineas = [l.strip('\n') for l in lineas]
+        mensajes = []
+        # mensaje = []
+        id_foro = ""
+        # id_asignatura = 12345678
+        nombreForo = ""
+        id_hilo = ""
+        tit_hilo = ""
+        id_mensaje = ""
+        id_ref_mensaje = ""
+        id_autor = ""
+        autor = ""
+        fecha = ""
+        dia_semana = ""
+        tit_mensaje = ""
+        texto = ""
+        estado = 'FIN_MENSAJE'
+
+        for index, linea in enumerate(lineas):
+
+            if linea.startswith('Foro: '):
+                print(linea.partition('Foro: ')[2])
+                nombreForo = linea.partition('Foro: ')[2]
+                id_foro = int(hashlib.sha1(nombreForo.encode('utf-8')).hexdigest(), 16) % (10 ** 8)
+
+            elif linea.startswith('Mensajes de la conversación: '):
+                tit_hilo = linea.partition('Mensajes de la conversación: ')[2]
+                id_hilo = int(hashlib.sha1(tit_hilo.encode('utf-8')).hexdigest(), 16) % (10 ** 8)
+
+            elif linea.startswith('Mensaje no. '):
+                if ' (Respuesta a no. ' in linea:
+                    respuesta = linea.partition(' (Respuesta a no. ')[2].partition(")")[0]
+                    no_mensaje = linea.partition('Mensaje no. ')[2].partition(' (Respuesta a no. ')[0]
+                    id_mensaje = str(id_hilo) + "_" + no_mensaje
+                    id_ref_mensaje = str(id_hilo) + "_" + respuesta
+
+                else:
+                    no_mensaje = linea.partition('Mensaje no. ')[2]
+                    id_mensaje = str(id_hilo) + "_" + no_mensaje
+                    id_ref_mensaje = ""
+                # print(no_mensaje)
+
+            elif linea.startswith('Enviado por: '):
+                autor = linea.partition('Enviado por: ')[2].partition(" el")[0]
+                id_autor = int(hashlib.sha1(autor.encode('utf-8')).hexdigest(), 16) % (10 ** 8)
+                fecha = linea.partition(" el ")[2].strip()
+                if not lineas[index + 1].startswith('Título: '):
+                    # print('bien, estado: ' + estado)
+                    fecha = fecha + " " + lineas[index + 1]
+                    # print(fecha)
+                # else:
+                #    print('bien : ' + fecha)
+                # print(fecha)
+
+            elif linea.startswith('Título: '):
+                # print(lineas[lineas.index(linea)-1])
+                # if not lineas[lineas.index(linea)-1].startswith('Enviado por: '):
+                #   fecha = fecha.strip() + ' ' + lineas[lineas.index(linea)-1].strip()
+                #  print(lineas[lineas.index(linea)-1], fecha)
+                # print(linea)
+                if no_mensaje == "1":
+                    tit_hilo = linea.partition('Título: ')[2]
+                tit_mensaje = linea.partition('Título: ')[2]
+
+            elif linea.startswith('----------------------------------------------------------------------'):
+                estado = 'FIN_MENSAJE'
+                if autor:
+                    fecha_parser = fecha.split(' ')
+                    dia_semana = fecha_parser[0]
+                    fecha = fecha_parser[1] + "/" + str(month_string_to_number(fecha_parser[2])) + "/" + fecha_parser[3]
+                    hora = fecha_parser[4]
+                    # mensaje=[tit_hilo, id_hilo, id_mensaje, id_ref_mensaje, id_autor,autor,  dia_semana, fecha, tit_mensaje, texto]
+                    mensaje = {'Foro': id_foro, 'ForoN': nombreForo, 'Asignatura': id_asig, 'Título': tit_hilo,
+                               'Hilo': id_hilo,
+                               'Mensaje': id_mensaje, 'Responde a': id_ref_mensaje,
+                               'Remitente': id_autor, 'Autor': autor, 'Día': dia_semana, 'Fecha': fecha, 'Hora': hora,
+                               'Título mensaje': tit_mensaje,
+                               'Texto mensaje': texto.strip(), 'Caracteres mensaje': len(texto)}
+                    mensajes.append(mensaje)
+                    texto = ""
+                    autor = ""
+
+                    # print(linea)
+            elif linea.startswith('==============================================================================='):
+                estado = 'FIN_MENSAJE'
+                if autor:
+                    fecha_parser = fecha.split(' ')
+                    dia_semana = fecha_parser[0]
+                    fecha = fecha_parser[1] + "/" + str(month_string_to_number(fecha_parser[2])) + "/" + fecha_parser[3]
+                    hora = fecha_parser[4].strip()
+                    # mensaje=[tit_hilo, id_hilo, id_mensaje, id_ref_mensaje, id_autor,autor,  dia_semana, fecha, tit_mensaje, texto]
+                    mensaje = {'Foro': id_foro, 'ForoN': nombreForo, 'Asignatura': id_asig, 'Título': tit_hilo,
+                               'Hilo': id_hilo,
+                               'Mensaje': id_mensaje, 'Responde a': id_ref_mensaje,
+                               'Remitente': id_autor, 'Autor': autor, 'Día': dia_semana, 'Fecha': fecha, 'Hora': hora,
+                               'Título mensaje': tit_mensaje,
+                               'Texto mensaje': texto.strip(), 'Caracteres mensaje': len(texto)}
+                    mensajes.append(mensaje)
+                    texto = ""
+                    autor = ""
+                    # print(linea)
+
+            if lineas[index - 1].startswith('Título: '):
+                estado = 'MENSAJE'
+
+            if estado == 'MENSAJE':
+                texto = texto + ' ' + linea
+                
+    return mensajes
+    # for msj in mensajes:
+    #    print(msj)
+
+
+def generar_csv(nombre_txt, lista_mensajes):
+
+    ruta_csv = nombre_txt + '.csv'
+    keys = lista_mensajes[0].keys()
+
+    with open(ruta_csv, 'w') as output_file:  # Just use 'w' mode in 3.x
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(lista_mensajes)
+    print('Fichero CSV general creado: ' + ruta_csv)
+    return ruta_csv

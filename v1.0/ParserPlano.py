707d48939af35d1c5831ecafdf687f2d8aba5e88
FJ <fsanchez@lsi.uned.es>
2019-12-10 08:49:32 +0100

    Repara conflicto de formato UTF-8 al escribir en el CSV. Error: 'charmap' codec can't encode characters

--- v1.0/ParserPlano.py
+++ v1.0/ParserPlano.py
@@ -154,9 +154,13 @@ def generar_csv(nombre_txt, lista_mensajes):
     ruta_csv = nombre_txt + '.csv'
     keys = lista_mensajes[0].keys()
 
-    with open(ruta_csv, 'w') as output_file:  # Just use 'w' mode in 3.x
-        dict_writer = csv.DictWriter(output_file, keys)
-        dict_writer.writeheader()
-        dict_writer.writerows(lista_mensajes)
+    with open(ruta_csv, 'w', encoding='utf-8') as output_file:  # Just use 'w' mode in 3.x
+        try:
+            dict_writer = csv.DictWriter(output_file, keys)
+            print(dict_writer, lista_mensajes, '\n', keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(lista_mensajes)
+        except Exception as e:
+            print(e)
     print('Fichero CSV general creado: ' + ruta_csv)
     return ruta_csv
